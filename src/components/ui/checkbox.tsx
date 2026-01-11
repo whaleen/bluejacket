10695734fbe7a5c83544c4685910ffd50a8334a8
whaleen <joshuavaage@gmail.com>
2026-01-11 13:42:03 -0800

    using shadcn canary now

--- src/components/ui/checkbox.tsx
+++ src/components/ui/checkbox.tsx
@@ -1,5 +1,3 @@
-"use client"
-
 import * as React from "react"
 import * as CheckboxPrimitive from "@radix-ui/react-checkbox"
 import { CheckIcon } from "lucide-react"
@@ -21,7 +19,7 @@ function Checkbox({
     >
       <CheckboxPrimitive.Indicator
         data-slot="checkbox-indicator"
-        className="flex items-center justify-center text-current transition-none"
+        className="grid place-content-center text-current transition-none"
       >
         <CheckIcon className="size-3.5" />
       </CheckboxPrimitive.Indicator>
